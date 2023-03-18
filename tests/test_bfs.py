66f72bd7d4db98bf48203e1c77983b9dcd151cbd
TimPushkin <pushkin.td@gmail.com>
2023-03-18 13:24:28 +0300

    Implements MSBFS

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -36,3 +36,40 @@ def test_bfs_correct_inputs(graph: gb.Matrix, start: int, expected: list[int]):
     actual = bfs.bfs(graph, start)
 
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "graph, starts",
+    utils.load_test_data(
+        "test_msbfs_incorrect_inputs",
+        lambda d: (
+            utils.matrix_from_dense_list(
+                d["graph"],
+                gb.types.BOOL if d["matrix_type"] == "BOOL" else gb.types.INT64,
+            ),
+            d["starts"],
+        ),
+    ),
+    ids=utils.load_test_ids("test_msbfs_incorrect_inputs"),
+)
+def test_msbfs_incorrect_inputs(graph: gb.Matrix, starts: list[int]):
+    with pytest.raises(ValueError):
+        bfs.msbfs(graph, starts)
+
+
+@pytest.mark.parametrize(
+    "graph, starts, expected",
+    utils.load_test_data(
+        "test_msbfs_correct_inputs",
+        lambda d: (
+            utils.matrix_from_dense_list(d["graph"]),
+            d["starts"],
+            [tuple(pair_list) for pair_list in d["expected"]],
+        ),
+    ),
+    ids=utils.load_test_ids("test_msbfs_correct_inputs"),
+)
+def test_msbfs_correct_inputs(graph: gb.Matrix, starts: list[int], expected: list[int]):
+    actual = bfs.msbfs(graph, starts)
+
+    assert actual == expected
